26aa276eac10f3003c3a3ad65e202e04f7a506af
Joel R <129562199+joelrennert@users.noreply.github.com>
2024-11-09 18:55:10 -0500

    first

---
+++ src/components/SquareScales.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="square"><a href="#"></a></div>
+</template>
+
+<script></script>
+
+<style scoped>
+.square {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  border-radius: 25%;
+  box-shadow:
+    8px 8px 18px rgba(0, 0, 0, 0.348),
+    -8px -8px 18px rgba(218, 215, 191, 0.6);
+  transform: rotate(100deg) skewX(10deg) skewY(10deg);
+  animation: pulse 4s ease-in-out infinite;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(1.3) rotate(100deg) skewX(10deg) skewY(10deg);
+  }
+  50% {
+    transform: scale(1.38) rotate(100deg) skewX(10deg) skewY(10deg);
+  }
+  100% {
+    transform: scale(1.3) rotate(100deg) skewX(10deg) skewY(10deg);
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,49 @@
+<script setup>
+import SquareScales from '../components/SquareScales.vue'
+import TheFooter from '@/components/TheFooter.vue'
+</script>
+
+<template>
+  <main>
+    <SquareScales v-for="n in 200" :key="n" class="square" />
+    <TheFooter />
+  </main>
+</template>
+
+<style scoped>
+.square {
+  padding: 48px;
+}
+
+main {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(218, 215, 191, 0.802);
+  gap: 40px;
+  overflow-y: hidden;
+  overflow-x: hidden;
+  font-family: 'IBM Plex Mono';
+  color: #333;
+
+  border-radius: 6px;
+
+  box-shadow:
+    0 4px 12px rgba(0, 0, 0, 0.321),
+    0 2px 4px rgba(0, 0, 0, 0.263);
+}
+
+main::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
